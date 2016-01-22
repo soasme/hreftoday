e46f47eb63693a06a1889f3b130230f016eaaa3a
soasme <soasme@gmail.com>
2016-01-22 22:01:51 +0800

    define follow topic endpoint.

--- app/views/web/topics.py
+++ app/views/web/topics.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import url_for, redirect, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
+from sqlalchemy.exc import IntegrityError
 from app.utils.forms import form_required, populate_obj
-from app.models import Topic
+from app.models import Topic, TopicFollow
 from app.forms import TopicForm
+from app.core import db
 from .core import bp
 
 @bp.route('/topics', defaults={'page': 1})
@@ -26,6 +28,18 @@ def add_topic():
 def update_topic_page(id):
     topic = Topic.query.get_or_404(id)
     return render_template('web/topic/update.html', topic=topic, form=TopicForm(obj=topic))
+
+@bp.route('/topics/<int:id>/follow')
+@login_required
+def follow_topic(id):
+    try:
+        topic = Topic.query.get_or_404(id)
+        follow = TopicFollow(user_id=current_user.id, topic_id=topic.id)
+        db.session.add(follow)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+    return redirect(url_for('web.get_topic', id=topic.id))
 
 @bp.route('/topics/<int:id>/update', methods=['POST'])
 @form_required(TopicForm)
