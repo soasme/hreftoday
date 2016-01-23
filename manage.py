e7fef58d9008d68881e7a96f0fdabf1a0f3a93e2
soasme <soasme@gmail.com>
2016-01-23 13:55:13 +0800

    add db command.

--- manage.py
+++ manage.py
@@ -2,13 +2,17 @@
 
 import os
 from app.application import create_app
+from app.core import db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app()
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 port = int(os.environ.get('PORT', 5000))
 manager.add_command("runserver", Server(host='0.0.0.0', port=port))
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def collectstatic():
