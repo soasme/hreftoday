50997b786df27e05065e0bbafbdc4bbfdc903946
soasme <soasme@gmail.com>
2016-01-23 13:53:30 +0800

    add filters.

---
+++ app/utils/filters.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def datetimeformat(value, format="%Y-%m-%d %H:%I:%S"):
+    return value.strftime(format)
+
+def dateformat(value):
+    return datetimeformat(value, "%Y-%m-%d")
+
+FILTERS = globals()
