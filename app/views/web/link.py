31da7b4dd5e8a92907657d059a6d7a0fcdabea1f
soasme <soasme@gmail.com>
2016-01-23 19:01:16 +0800

    Refactor link module.

--- app/views/web/link.py
+++ app/views/web/link.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, url_for, redirect
-from flask_login import current_user
-from flask_wtf import Form
+from flask_login import current_user, login_required
+from app.core import db
+from app.utils.transaction import transaction
+from app.utils.view import ensure_resource, templated
+from app.utils.forms import save_form_obj
 from app.models import Link, Tag, LinkTag, Issue
-from app.utils.forms import form_required, populate_obj
 from app.forms import LinkForm
+
 from .core import bp
 
 @bp.route('/links/<int:id>')
@@ -17,29 +20,26 @@ def get_link(id):
     tags = Tag.query.filter(Tag.id.in_(tag_ids)).all() if tag_ids else []
     return render_template('web/link/item.html', link=link, tags=tags, issue=issue)
 
-@bp.route('/issues/<int:id>/links')
-def add_issue_link_page(id):
-    form = AddLinkForm(data=dict(issue_id=id))
-    return render_template('web/link/add.html', form=form)
+@bp.route('/issues/<int:id>/links', methods=['GET', 'POST'])
+@transaction(db)
+@login_required
+@templated('web/link/add.html')
+@ensure_resource(Issue)
+def add_issue_link(id, issue):
+    link = Link(user_id=current_user.id, issue_id=issue.id)
+    return save_form_obj(
+        db, LinkForm, link,
+        build_next=lambda form, link: url_for('web.get_link', id=link.id)
+    )
 
-@bp.route('/issues/<int:id>/links', methods=['POST'])
-@form_required(AddLinkForm)
-def add_issue_link(id):
-    link = Link(user_id=current_user.id)
-    return _save_link(link)
-
-@bp.route('/links/<int:id>/update')
-def update_link_page(id):
-    link = Link.query.get_or_404(id)
-    form = EditLinkForm(obj=link)
-    return render_template('web/link/update.html', link=link, form=form)
-
-@bp.route('/links/<int:id>/update', methods=['POST'])
-@form_required(EditLinkForm)
-def update_link(id):
-    link = Link.query.get_or_404(id)
-    return _save_link(link)
-
-def _save_link(link):
-    populate_obj(link)
-    return redirect(url_for('web.get_link', id=link.id))
+@bp.route('/links/<int:id>/update', methods=['GET', 'POST'])
+@transaction(db)
+@login_required
+@templated('web/link/update.html')
+@ensure_resource(Link)
+def update_link(id, link):
+    return save_form_obj(
+        db, LinkForm, link,
+        build_next=lambda form, link: url_for('web.get_link', id=link.id),
+        before_render_map=['obj->link'],
+    )
