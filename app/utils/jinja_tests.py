865797556b2072e65bfef622eaf63ebb7a8ba810
soasme <soasme@gmail.com>
2016-01-24 21:20:09 +0800

    Rename to is_belongs_to_me.

--- app/utils/jinja_tests.py
+++ app/utils/jinja_tests.py
@@ -2,7 +2,7 @@
 
 from flask_login import current_user
 
-def is_owned_by_current_user(obj):
+def is_belongs_to_me(obj):
     return obj and not current_user.is_anonymous and obj.user_id == current_user.id
 
 TESTS = {k[3:]: v for k, v in globals().items() if k.startswith('is_')}
