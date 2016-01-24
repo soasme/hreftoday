51c5df3d40e684c8b490a4e7084ca7526d74bb2a
soasme <soasme@gmail.com>
2016-01-24 21:19:58 +0800

    Modify dateformat from - to /.

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,6 +4,6 @@ def datetimeformat(value, format="%Y-%m-%d %H:%I:%S"):
     return value.strftime(format)
 
 def dateformat(value):
-    return datetimeformat(value, "%Y-%m-%d")
+    return datetimeformat(value, "%Y/%m/%d")
 
 FILTERS = globals()
