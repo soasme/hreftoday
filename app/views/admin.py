09f06ec75aa44137544faa319fec955c670dbd6e
soasme <soasme@gmail.com>
2016-01-29 12:56:15 +0800

    Send log on unaccessiable.

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from flask import abort
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
@@ -8,14 +9,24 @@ from app.models import Ad, Issue, Link
 
 # Flask and Flask-SQLAlchemy initialization here
 
+
 class AdminModelView(ModelView):
     can_delete = False
     page_size = 50
 
     def is_accessible(self):
+        admin_logger = logging.getLogger('app.admin')
         if not current_user.is_authenticated:
+            admin_logger.warning(
+                'an atempting entering from unauthenticated user: %s',
+                request.remote_addr,
+            )
             abort(403)
         if not current_user.has_role('admin'):
+            admin_logger.warning(
+                'an atempting entering from non-permissive user: %s',
+                current_user.id
+            )
             abort(403)
         return True
 
