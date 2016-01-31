7e61525648c1ec4c3afdb1a85ec68924a502e31b
soasme <soasme@gmail.com>
2016-01-31 16:24:44 +0800

    Add contact page.

--- app/views/web/info.py
+++ app/views/web/info.py
@@ -3,9 +3,15 @@
 from flask import render_template, current_app
 from .core import bp
 
-@bp.route('/aboutus')
-def aboutus():
+@bp.route('/about')
+def about():
     return render_template(
-        'web/aboutus.html',
+        'web/about.html',
+    )
+
+@bp.route('/contact')
+def contact():
+    return render_template(
+        'web/contact.html',
         contact_email=current_app.config.get('CONTACT_EMAIL')
     )
