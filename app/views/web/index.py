38378ce09dbf14f046dd388a9bea0b4d3f486917
soasme <soasme@gmail.com>
2016-02-01 14:02:30 +0800

    Jump to get topics.

--- app/views/web/index.py
+++ app/views/web/index.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, url_for, redirect
 from flask_user import login_required
 from .core import bp
 
 @bp.route('/')
 @login_required
 def index():
-    return render_template('web/index.html')
+    return redirect(url_for('web.get_topics'))
