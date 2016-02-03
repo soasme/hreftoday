3a9e5b4852cffab8e0847a040bbdbb3a377e5f8a
soasme <soasme@gmail.com>
2016-02-03 17:30:05 +0800

    Add acme-challenge page.

--- app/views/web/info.py
+++ app/views/web/info.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, current_app
+from flask import render_template, current_app, abort, make_response
 from .core import bp
 
 @bp.route('/about')
@@ -15,3 +15,11 @@ def contact():
         'web/contact.html',
         contact_email=current_app.config.get('CONTACT_EMAIL')
     )
+
+@bp.route('/.well-known/acme-challenge/<key>')
+def acme_challenge(key):
+    if key != current_app.config['ACME_CHALLENGE_KEY']:
+        abort(404)
+    resp = make_response(current_app.config['ACME_CHALLENGE_VALUE'])
+    resp.content_type = 'text/plain'
+    return resp
