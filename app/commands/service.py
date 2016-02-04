7fe620ada426a5976143d89ca8232b56d75cef95
soasme <soasme@gmail.com>
2016-02-04 20:21:35 +0800

    Remove issue publish/unpublish

--- app/commands/service.py
+++ app/commands/service.py
@@ -3,7 +3,7 @@
 from flask_script import Manager
 from datetime import datetime
 from app.core import db
-from app.models import Topic, Issue, User, Role
+from app.models import Topic, User, Role
 from app.utils.transaction import transaction
 
 Service = Manager('Perform service operation')
@@ -15,34 +15,6 @@ def revert_topic(id):
         topic.is_deleted = False
         db.session.commit()
         print 'Topic "%s" reverted' % topic.title
-    else:
-        print 'Not Found'
-
-@Service.command
-def publish_issue(id):
-    issue = Issue.query.get(id)
-    if issue:
-        latest_issue = Issue.query.filter_by(
-            topic_id=issue.topic_id
-        ).order_by(
-            Issue.serial.desc()
-        ).first()
-        current_topic_max_serial = latest_issue and latest_issue.serial or 0
-        issue.serial = 1 + current_topic_max_serial
-        issue.published_at = datetime.utcnow()
-        db.session.commit()
-        print 'Issue published as serial %s' % issue.serial
-    else:
-        print 'Not Found'
-
-@Service.command
-def unpublish_issue(id):
-    issue = Issue.query.get(id)
-    if issue:
-        issue.serial = None
-        issue.published_at = None
-        db.session.commit()
-        print 'Issue unpublished'
     else:
         print 'Not Found'
 
